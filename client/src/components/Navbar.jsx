8dbe39861ab395fed43b71d4fa9d2602fd0d8be8
Build-with-Akshit <akshitgupta9359@gmail.com>
2026-09-13 07:16:31 +0530

    style: update navbar brand logo to match location drop pin icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,7 +15,11 @@ export const Navbar = ({
   return (
     <header className="navbar">
       <div className="brand" onClick={onGoHome}>
-        <div className="brand-icon">⚡</div>
+        <div className="brand-icon">
+          <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
+          </svg>
+        </div>
         <div>
           <span className="brand-name">DropThing</span>
         </div>
